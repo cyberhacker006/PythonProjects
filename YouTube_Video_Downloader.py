906c6febaaed0f1b6d13a94f2554e6c158a4ab24
Berkay Güzel <98205992+cyberhacker006@users.noreply.github.com>
2022-04-13 00:22:54 +0300

    Update YouTube_Video_Downloader.py

--- YouTube_Video_Downloader.py
+++ YouTube_Video_Downloader.py
@@ -2,5 +2,5 @@ from pytube import YouTube
 link = input("Enter a video link: ")
 yt = YouTube(link)
 video = yt.streams.get_highest_resolution()
-save_path = r"C:\Users\Berkay\OneDrive\Masaüstü\Youtube"
+save_path = r"C:\Users\Youtube"
 video.download(save_path)
