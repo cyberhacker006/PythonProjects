b1f55032f905add739bb2adc25bac83704c5b636
Berkay Güzel <98205992+cyberhacker006@users.noreply.github.com>
2022-04-13 00:21:43 +0300

    Delete YouTube_Video_Downloader.py

--- YouTube_Video_Downloader.py
+++
@@ -1,6 +0,0 @@
-from pytube import YouTube
-link = input("Enter a video link: ")
-yt = YouTube(link)
-video = yt.streams.get_highest_resolution()
-save_path = r"C:\Users\Berkay\OneDrive\Masaüstü\Youtube"
-video.download(save_path)
