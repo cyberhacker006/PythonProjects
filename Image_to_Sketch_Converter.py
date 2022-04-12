e841330982f07f981a1f9cb7d88ecfc08c020fc8
cyberhacker006 <98205992+cyberhacker006@users.noreply.github.com>
2022-04-12 21:20:46 +0300

    Update Image_to_Sketch_Converter.py

--- Image_to_Sketch_Converter.py
+++ Image_to_Sketch_Converter.py
@@ -1,11 +1,11 @@
 import cv2 as cv
-image = cv.imread("image.png")
-cv.imshow("Image", image)
+image = cv.imread("image.png")                            // image that you want to convert
+cv.imshow("Image", image)                                 // show uploaded image
 gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 inverted = 255-gray_image
 blur=cv.GaussianBlur(inverted,(21,21),0)
 invertedBlur=255-blur
 sketch=cv.divide(gray_image,invertedBlur,scale=256.0)
 cv.imwrite("Sketch.jpg",sketch)
-cv.imshow("Sketch",sketch)
+cv.imshow("Sketch",sketch)                               // show converted image
 cv.waitKey(0)
